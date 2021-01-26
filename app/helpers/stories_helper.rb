a82f72a8f9a52ce2c4794ca0a9f9a6d64cef39c2
Alina Bychkova <ialina240200@gmail.com>
2021-01-26 20:34:27 +0300

    Add helper for output stories

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,8 @@
 module StoriesHelper
+  def author_info(story)
+    name = story.name ? story.name : 'Анонимно'
+    age = story.age ? ", #{story.age}" : ''
+    city = story.city ? ", #{story.city}" : ''
+    name + age + city
+  end
 end
