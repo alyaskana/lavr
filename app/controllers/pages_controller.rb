fb82febfd368fdfc6e6795b73df297f9f32b0112
Alina Bychkova <ialina240200@gmail.com>
2021-09-01 15:09:27 +0300

    Show all stories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
 
   # GET /
   def index
-    @stories = Story.where(is_published: true).order(id: :desc).limit(2)
+    @stories = Story.where(is_published: true).order(id: :desc)
   end
 end
